a31046250924f78098db717f1905037fdce94dad
Ivan Stevkovski <ivan@qbit.solutions>
2019-10-14 19:42:52 +0200

    Built production bundle and updated api routes for prod

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,7 +8,7 @@ export const bookTour = async tourId => {
     try {
         // Get checkout session from api
         const session = await axios(
-            `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+            `/api/v1/bookings/checkout-session/${tourId}`
         );
 
         // Create checkout form and charge process
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -10,7 +10,7 @@ export const updateSettings = async (data, type) => {
 
         const res = await axios({
             method: 'PATCH',
-            url: `http://127.0.0.1:3000/api/v1/users/${urlType}`,
+            url: `/api/v1/users/${urlType}`,
             data
         });
 
